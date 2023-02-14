b72e51140102c8e5d4757b7fc427cf823d1e739e
Bartlomiej Milewski <b.milewski762@gmail.com>
2023-02-14 15:44:14 +0100

    PostService created

--- src/main/java/com/example/Bloggy/controller/BlogPostController.java
+++ src/main/java/com/example/Bloggy/controller/BlogPostController.java
@@ -3,11 +3,14 @@ package com.example.Bloggy.controller;
 import com.example.Bloggy.exceptions.ResourceNotFoundException;
 import com.example.Bloggy.model.BlogPost;
 import com.example.Bloggy.repo.BlogPostRepository;
+import com.example.Bloggy.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,45 +19,44 @@ import java.util.Map;
 
 public class BlogPostController {
     @Autowired
-    private BlogPostRepository blogPostRepository;
+    private PostService postService;
+
+    public BlogPostController(PostService postService) {
+        this.postService = postService;
+    }
 
     @GetMapping(path = "/posts")
-    public @ResponseBody Iterable<BlogPost> getAllPosts() {
-        return blogPostRepository.findAll();
+    public ResponseEntity<List<BlogPost>> getAllPosts() {
+        List<BlogPost> posts = postService.getAllPosts();
+        return new ResponseEntity<>(posts, HttpStatus.OK);
     }
 
     @PostMapping(path = "/posts")
-    public BlogPost addPost(@RequestBody BlogPost blogPost){
-        return blogPostRepository.save(blogPost);
+    public ResponseEntity<BlogPost> addPost(@RequestBody BlogPost blogPost) {
+        BlogPost newPost = postService.addPost(blogPost);
+        return new ResponseEntity<>(newPost, HttpStatus.CREATED);
     }
 
     @GetMapping(path="/posts/{id}")
     public ResponseEntity<BlogPost> getPostById(@PathVariable Integer id){
-        BlogPost blogPost = blogPostRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " does not exist"));
-        return ResponseEntity.ok(blogPost);
+        BlogPost blogPost = postService.findPostById(id);
+        return new ResponseEntity<>(blogPost, HttpStatus.OK);
     }
     @PutMapping(path="/posts/{id}")
-    public ResponseEntity<BlogPost> updatePost(@PathVariable Integer id, @RequestBody BlogPost blogPostDetails){
-        BlogPost blogPost = blogPostRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " does not exist"));
+    public ResponseEntity<BlogPost> updatePost(@PathVariable Integer id, @RequestBody BlogPost blogPostDetails) {
+        BlogPost blogPost = postService.findPostById(id);
 
         blogPost.setAuthor(blogPostDetails.getAuthor());
         blogPost.setTags(blogPostDetails.getTags());
         blogPost.setTitle(blogPostDetails.getTitle());
         blogPost.setContent(blogPostDetails.getContent());
 
-        BlogPost updatedPost = blogPostRepository.save(blogPost);
-        return ResponseEntity.ok(updatedPost);
+        BlogPost updatedPost = postService.updatePost(blogPost);
+        return new ResponseEntity<>(updatedPost, HttpStatus.OK);
     }
     @DeleteMapping(path ="/posts/{id}")
-    public ResponseEntity<Map<String, Boolean>> deletePost(@PathVariable Integer id){
-
-        BlogPost blogPost = blogPostRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " does not exist"));
-        blogPostRepository.delete(blogPost);
-        Map<String, Boolean> response = new HashMap<>();
-        response.put("Post with id "+ id + " deleted", Boolean.TRUE);
-        return ResponseEntity.ok(response);
+    public ResponseEntity<BlogPost> deletePostById(@PathVariable Integer id) {
+        postService.deletePost(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
---
+++ src/main/java/com/example/Bloggy/service/PostService.java
@@ -0,0 +1,44 @@
+package com.example.Bloggy.service;
+
+import com.example.Bloggy.exceptions.ResourceNotFoundException;
+import com.example.Bloggy.model.BlogPost;
+import com.example.Bloggy.repo.BlogPostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    private final BlogPostRepository blogPostRepository;
+
+    @Autowired
+    public PostService(BlogPostRepository blogPostRepository) {
+        this.blogPostRepository = blogPostRepository;
+    }
+
+    public BlogPost addPost(BlogPost blogPost) {
+        return blogPostRepository.save(blogPost);
+    }
+
+    public List<BlogPost> getAllPosts() {
+        return blogPostRepository.findAll();
+    }
+
+    public BlogPost updatePost(BlogPost blogPost) {
+        return blogPostRepository.save(blogPost);
+    }
+
+    public BlogPost findPostById(Integer id) {
+        return blogPostRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " does not exist"));
+    }
+
+    public void deletePost(Integer id) {
+        BlogPost blogPost = blogPostRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Post with id " + id + " does not exist"));
+        blogPostRepository.deleteById(id);
+    }
+
+
+}
